0334920e1b0e698ad94a8a01a77777ab8db2527e
al cabrera <chyrill.cabrera@gmail.com>
2018-06-26 14:08:14 +0800

    edited package.json and artwork details component

--- components/viewartwork/artworkdetailscomp.vue
+++ components/viewartwork/artworkdetailscomp.vue
@@ -53,16 +53,6 @@
 <script>
 /* eslint-disable */
 
-function getRemainingTime(startDate, end) {
-    
-    var diff = (end - startDate)/1000
-    var days = Math.floor(diff/ (60 * 60 * 24))
-    var hours = Math.floor((diff - (days * 60 * 60 * 24))/(60 * 60))
-    var minutes = Math.floor((diff - ((days * 60 * 60 * 24) + (hours * 60 * 60)))/ 60)
-    var seconds = Math.floor((diff - ((days * 60 * 60 * 24) + (hours * 60 * 60)+(minutes * 60))))
-    return days +':' + hours + ':' + minutes + ':' + seconds
-}   
-
 export default {
     props: {
        AuctionDate: {
